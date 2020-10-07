9a2ea1311e2a282606dd6d33cde419c4fb879fbc
Althaf Hussain Shaik <iamalthaf003@gmail.com>
2020-10-07 20:31:39 +0530

    single page applications part-2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,53 +1,67 @@
 import React from 'react';
-import { Card, CardImg , CardTitle, CardText, CardBody} from 'reactstrap';
-
+import { Card, CardImg , CardTitle, CardText, CardBody ,Breadcrumb ,BreadcrumbItem} from 'reactstrap';
+import { Link } from 'react-router-dom' ;
 
 function RenderDish({dish}){
     return (
-            <Card key={dish.id}>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <div className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText> {dish.description} </CardText>
                 </CardBody>
             </Card>
+        </div>
     );
 }
 
 function RenderComments({comments}) {
-    return(
-        <ul>
-            {comments.map((comment)=>{
-                    return (
-                        <li key={comment.id}>
-                            <p>{comment.comment}</p>
-                            <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                        </li>
-                    );
-                    })}                
-        </ul>                  
-    );  
+    if (comments != null)
+        return(
+            <div className="col-12 col-md-5 m-1">
+                <ul>
+                    {comments.map((comment)=>{
+                            return (
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                            );
+                            })}                
+                </ul>
+            </div>                  
+        );
+    else
+        return(
+            <div></div>
+        );
 };
 
 const DishDetail = (props)=> {
- if (props.dish != null){
+ if (props.dish != null)
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-md-5 col-sm-12 m-1">
-                        <RenderDish dish={props.dish} />
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link> </BreadcrumbItem> 
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
                     </div>
-                    <div className="col-md-5 col-sm-12 m-1">
-                        <RenderComments comments={props.dish.comments} />
-                    </div>
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         );
-    }else {
+    else
         return (
             <div></div>
         );
-    }
 
 }
 
