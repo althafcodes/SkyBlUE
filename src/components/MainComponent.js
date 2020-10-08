3460ca681c06b29df633ed43cff899d50520a8e6
Althaf Hussain Shaik <iamalthaf003@gmail.com>
2020-10-08 13:27:32 +0530

    controlled forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponet';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
@@ -10,6 +11,7 @@ import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
 import {Switch, Route, Redirect} from 'react-router-dom';
+
 
 
 class Main extends Component {
@@ -46,6 +48,7 @@ class Main extends Component {
           <Header />
             <Switch>
               <Route path='/home' component={HomePage} />
+              <Route exact path='/aboutus' component={()=> <About leaders={this.state.leaders}/>} />
               <Route exact path='/menu' component={()=> <Menu dishes={this.state.dishes} /> } />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
