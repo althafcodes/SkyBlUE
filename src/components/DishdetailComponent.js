0dd38d8f4fecbbbab9361948408950d6f9e9602a
Althaf Hussain Shaik <iamalthaf003@gmail.com>
2020-10-16 21:06:35 +0530

    REDUX  THUNK

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Card, CardImg , CardTitle, CardText, CardBody ,Breadcrumb ,BreadcrumbItem,Modal,Button, Col,Label, Row,ModalHeader,ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom' ;
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading} from './LoadingComponent';
 
 function RenderDish({dish}){
     return (
@@ -132,12 +133,30 @@ class CommentForm extends Component {
 }
 
 const DishDetail = (props)=> {
- if (props.dish != null)
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null)
         return (
             <div className="container">
                 <div className="row">
                     <Breadcrumb>
-                        <BreadcrumbItem><Link to='/menu'>Menu</Link> </BreadcrumbItem> 
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link> </BreadcrumbItem>
                         <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
                     </Breadcrumb>
                     <div className="col-12">
